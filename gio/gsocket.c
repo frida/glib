d1b7510e9a6e918bf8161a4ba5475c0621452bc6
Ole André Vadla Ravnås <oleavr@gmail.com>
2010-09-26 15:06:25 +0200

    Don't call WSAEnumNetworkEvents if socket is closed.
    Makes Application Verifier happy.

--- gio/gsocket.c
+++ gio/gsocket.c
@@ -3153,7 +3153,8 @@ update_condition (GSocket *socket)
   WSANETWORKEVENTS events;
   GIOCondition condition;
 
-  if (WSAEnumNetworkEvents (socket->priv->fd,
+  if (!socket->priv->closed &&
+      WSAEnumNetworkEvents (socket->priv->fd,
 			    socket->priv->event,
 			    &events) == 0)
     {
