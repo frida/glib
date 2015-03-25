a1771a85c6e516c5a0091b8cd1aaa6355d72079b
s1341 <github@shmarya.net>
2015-03-25 17:54:03 +0200

    gunixmounts: add stubs for QNX

--- gio/gunixmounts.c
+++ gio/gunixmounts.c
@@ -949,6 +949,21 @@ _g_get_unix_mounts (void)
   return return_list;
 }
 
+/* QNX {{{2 */
+#elif defined(HAVE_QNX)
+
+static char *
+get_mtab_monitor_file (void)
+{
+  return NULL;
+}
+
+static GList *
+_g_get_unix_mounts (void)
+{
+  return NULL;
+}
+
 /* Common code {{{2 */
 #else
 #error No _g_get_unix_mounts() implementation for system
@@ -1466,6 +1481,14 @@ _g_get_unix_mount_points (void)
 }
 /* Interix {{{2 */
 #elif defined(__INTERIX)
+static GList *
+_g_get_unix_mount_points (void)
+{
+  return _g_get_unix_mounts ();
+}
+
+/* QNX {{{2 */
+#elif defined(HAVE_QNX)
 static GList *
 _g_get_unix_mount_points (void)
 {
