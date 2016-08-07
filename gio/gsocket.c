0bdde99f30dd3445f33d4cca6a19a41b9b7495d3
s1341 <github@shmarya.net>
2016-08-07 18:31:56 +0200

    gsocket: Decrease msg_control size for QNX
    The reduction of 32 bytes should not matter for the currently supported
    OSes.

--- gio/gsocket.c
+++ gio/gsocket.c
@@ -4625,7 +4625,7 @@ G_STMT_START { \
     } \
   else \
     { \
-      _msg->msg_controllen = 2048; \
+      _msg->msg_controllen = 2016; /* upper limit on QNX */ \
       _msg->msg_control = g_alloca (_msg->msg_controllen); \
     } \
  \
