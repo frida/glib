b013a1d23246bc7555808b48f2ac6612a3b5c7ca
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-03-13 17:27:52 +0100

    Add support for dynamic unloading and fork()-handling
    When GLib is built as a static library, constructors and destructor
    functions are queued up until `glib_[de]init()`. This way the
    application may override the heap VTable before GLib starts allocating
    memory, which means it is able to release all memory allocated by GLib
    without explicit deinit code for every single statically allocated block
    of memory. What this means is that only OS handles are left to be
    cleaned up, and that's where `glib_deinit()` needs to perform explicit
    clean-up.
    
    This is a pragmatic solution to support linking GLib into short-lived
    plugins. The goal is to keep this patch as minimal as possible, so it's
    easy to keep maintaining it for future GLib versions. (Upstream has no
    interest in this use-case.)
    
    Note that `glib_init()` and `glib_deinit()` do absolutely nothing unless
    GLib was compiled as a static library.
    
    We also embed a private proxy-libintl for static builds.

--- gobject/gtype.c
+++ gobject/gtype.c
@@ -4410,12 +4410,17 @@ g_type_init (void)
 }
 
 static void
-gobject_init (void)
+gobject_perform_init (void)
 {
+  static gboolean initialized = FALSE;
   const gchar *env_string;
   GTypeInfo info;
   TypeNode *node;
   GType type G_GNUC_UNUSED  /* when compiling with G_DISABLE_ASSERT */;
+
+  if (initialized)
+    return;
+  initialized = TRUE;
 
   /* Ensure GLib is initialized first, see
    * https://bugzilla.gnome.org/show_bug.cgi?id=756139
@@ -4504,7 +4509,15 @@ gobject_init (void)
   _g_signal_init ();
 }
 
-#if defined (G_OS_WIN32)
+void
+gobject_init (void)
+{
+#ifdef GLIB_STATIC_COMPILATION
+  gobject_perform_init ();
+#endif
+}
+
+#if defined (G_OS_WIN32) && !defined (GLIB_STATIC_COMPILATION)
 
 BOOL WINAPI DllMain (HINSTANCE hinstDLL,
                      DWORD     fdwReason,
@@ -4518,7 +4531,7 @@ DllMain (HINSTANCE hinstDLL,
   switch (fdwReason)
     {
     case DLL_PROCESS_ATTACH:
-      gobject_init ();
+      gobject_perform_init ();
       break;
 
     default:
@@ -4538,7 +4551,7 @@ G_DEFINE_CONSTRUCTOR(gobject_init_ctor)
 static void
 gobject_init_ctor (void)
 {
-  gobject_init ();
+  gobject_perform_init ();
 }
 
 #else
