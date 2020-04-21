a1ff21b0f912144cd5ab9c06c4dd6ce1b146b7e3
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-04-21 03:47:14 +0000

    Remove noisy debug logging

--- gio/gnetworkaddress.c
+++ gio/gnetworkaddress.c
@@ -1245,8 +1245,6 @@ got_ipv6_addresses (GObject      *source_object,
   addresses = g_resolver_lookup_by_name_with_flags_finish (resolver, result, &error);
   if (!error)
     g_network_address_address_enumerator_add_addresses (addr_enum, g_steal_pointer (&addresses), resolver);
-  else
-    g_debug ("IPv6 DNS error: %s", error->message);
 
   /* If ipv4 was first and waiting on us it can stop waiting */
   if (addr_enum->wait_source)
@@ -1301,8 +1299,6 @@ got_ipv4_addresses (GObject      *source_object,
   addresses = g_resolver_lookup_by_name_with_flags_finish (resolver, result, &error);
   if (!error)
     g_network_address_address_enumerator_add_addresses (addr_enum, g_steal_pointer (&addresses), resolver);
-  else
-    g_debug ("IPv4 DNS error: %s", error->message);
 
   if (addr_enum->wait_source)
     {
