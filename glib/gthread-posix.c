1dcd71ceca3b01da589a2002fd68c4be0c73c0e2
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-04-21 03:47:14 +0000

    Remove noisy debug logging

--- glib/gthread-posix.c
+++ glib/gthread-posix.c
@@ -1282,7 +1282,6 @@ g_system_thread_get_scheduler_settings (GThreadSchedulerSettings *scheduler_sett
             }
           else
             {
-              g_debug ("Failed to get thread scheduler attributes: %s", g_strerror (errsv));
               g_free (scheduler_settings->attr);
 
               return FALSE;
@@ -1298,7 +1297,6 @@ g_system_thread_get_scheduler_settings (GThreadSchedulerSettings *scheduler_sett
     {
       int errsv = errno;
 
-      g_debug ("Failed to set thread scheduler attributes: %s", g_strerror (errsv));
       g_free (scheduler_settings->attr);
 
       return FALSE;
