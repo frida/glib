frida/glib

commit 39f047e8213bc0dad15171397b3af16151a4a67c

gsocket: Add missing check_socket() checks
This makes no great difference, just increases robustness a bit.

Signed-off-by: Philip Withnall <withnall@endlessm.com>

Closes: #1339

gio/gsocket.c
@@ -3117,6 +3117,9 @@ g_socket_get_available_bytes (GSocket *socket)
 
   g_return_val_if_fail (G_IS_SOCKET (socket), -1);
 
+  if (!check_socket (socket, NULL))
+    return -1;
+
 #ifdef SO_NREAD
   if (!g_socket_get_option (socket, SOL_SOCKET, SO_NREAD, &avail, NULL))
       return -1;
@@ -3745,7 +3748,6 @@ g_socket_is_closed (GSocket *socket)
   return socket->priv->closed;
 }
 
-#ifdef G_OS_WIN32
 /* Broken source, used on errors */
 static gboolean
 broken_dispatch (GSource     *source,
@@ -3763,6 +3765,7 @@ static GSourceFuncs broken_funcs =
   NULL
 };
 
+#ifdef G_OS_WIN32
 static gint
 network_events_for_condition (GIOCondition condition)
 {
@@ -4089,6 +4092,12 @@ socket_source_new (GSocket      *socket,
       return g_source_new (&broken_funcs, sizeof (GSource));
     }
 #endif
+
+  if (!check_socket (socket, NULL))
+    {
+      g_warning ("Socket check failed");
+      return g_source_new (&broken_funcs, sizeof (GSource));
+    }
 
   condition |= G_IO_HUP | G_IO_ERR | G_IO_NVAL;
 
@@ -5931,6 +5940,9 @@ g_socket_get_credentials (GSocket   *socket,
   g_return_val_if_fail (G_IS_SOCKET (socket), NULL);
   g_return_val_if_fail (error == NULL || *error == NULL, NULL);
 
+  if (!check_socket (socket, error))
+    return NULL;
+
   ret = NULL;
 
 #if G_CREDENTIALS_SOCKET_GET_CREDENTIALS_SUPPORTED
@@ -6082,6 +6094,11 @@ g_socket_get_option (GSocket  *socket,
 
   g_return_val_if_fail (G_IS_SOCKET (socket), FALSE);
 
+  /* g_socket_get_option() is called during socket init, so skip the init checks
+   * in check_socket() */
+  if (socket->priv->inited && !check_socket (socket, error))
+    return FALSE;
+
   *value = 0;
   size = sizeof (gint);
   if (getsockopt (socket->priv->fd, level, optname, value, &size) != 0)
@@ -6144,6 +6161,9 @@ g_socket_set_option (GSocket  *socket,
   gint errsv;
 
   g_return_val_if_fail (G_IS_SOCKET (socket), FALSE);
+
+  if (!check_socket (socket, error))
+    return FALSE;
 
   if (setsockopt (socket->priv->fd, level, optname, &value, sizeof (gint)) == 0)
     return TRUE;

commit 835c67cfe7daf4a954867b735d250e2cbd44dddb

gsocket: Fix SO_NOSIGPIPE regression on Darwin
Where the early call to g_socket_set_option() fails because of
check_socket() failing due to `inited` still being FALSE.

This brings 634b692 back into working order, by fixing the regression
introduced in 39f047e.

Co-authored-by: Ole André Vadla Ravnås <oleavr@gmail.com>

gio/gsocket.c
@@ -6243,7 +6243,9 @@ g_socket_set_option (GSocket  *socket,
 
   g_return_val_if_fail (G_IS_SOCKET (socket), FALSE);
 
-  if (!check_socket (socket, error))
+  /* g_socket_set_option() is called during socket init, so skip the init checks
+   * in check_socket() */
+  if (socket->priv->inited && !check_socket (socket, error))
     return FALSE;
 
   if (setsockopt (socket->priv->fd, level, optname, &value, sizeof (gint)) == 0)

gio/tests/socket.c
@@ -1899,6 +1899,28 @@ test_read_write (gconstpointer user_data)
   g_object_unref (client);
 }
 
+#ifdef SO_NOSIGPIPE
+static void
+test_nosigpipe (void)
+{
+  GSocket *sock;
+  GError *error = NULL;
+  gint value;
+
+  sock = g_socket_new (AF_INET,
+                       G_SOCKET_TYPE_STREAM,
+                       G_SOCKET_PROTOCOL_DEFAULT,
+                       &error);
+  g_assert_no_error (error);
+
+  g_socket_get_option (sock, SOL_SOCKET, SO_NOSIGPIPE, &value, &error);
+  g_assert_no_error (error);
+  g_assert_true (value);
+
+  g_object_unref (sock);
+}
+#endif
+
 #if G_CREDENTIALS_SUPPORTED
 static gpointer client_setup_thread (gpointer user_data);
 
@@ -2165,6 +2187,9 @@ main (int   argc,
                         test_read_write);
   g_test_add_data_func ("/socket/read_writev", GUINT_TO_POINTER (TRUE),
                         test_read_write);
+#ifdef SO_NOSIGPIPE
+  g_test_add_func ("/socket/nosigpipe", test_nosigpipe);
+#endif
 #if G_CREDENTIALS_SUPPORTED
   g_test_add_func ("/socket/credentials/tcp_client", test_credentials_tcp_client);
   g_test_add_func ("/socket/credentials/tcp_server", test_credentials_tcp_server);